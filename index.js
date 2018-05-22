bac29a9790e82cf7a84c6eb40742dbf6dc40139e
shakir <muhammed_shakkirtk@infosys.com>
2018-05-22 16:48:07 +0530

    working copy

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-s
+/**
+ * mango
+ *
+ * built on top of mongodb client module, this nodejs module 
+ * will help to connect multiple collection from multiple db 
+ * instances.
+ *
+ * Please help me make this better:
+ * https://github.com/shakkirptb/mango
+ */
+
+module.exports =  require("./lib/mango-aliyan");
